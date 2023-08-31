246b2659c74306ef4fe93304e7a77280914f8244
xingye1234 <1989163733@qq.com>
2023-08-31 16:36:30 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import router from './router/index';
+import pinia from './store';
 import App from './App.vue';
 import './main.less';
 
 
 const app = createApp(App);
 app.use(router);
+app.use(pinia)
 app.mount('#app');
---
+++ src/store/user.ts
@@ -0,0 +1,20 @@
+import { defineStore} from 'pinia';
+
+export  const userStore = defineStore('userStore', () => {
+  const firstName = ref('w')
+  const lastName = ref('wkx')
+  
+
+  function setFirstName(params:any){
+    firstName.value = params
+  }
+  function setLastName(params:any){
+    lastName.value = params
+  }
+
+  const fullName = computed(()=>{
+    return firstName.value + lastName.value
+  })
+
+  return {firstName, lastName, setFirstName, setLastName, fullName}
+})
